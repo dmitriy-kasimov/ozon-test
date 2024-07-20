a16d0ef8191beac8dc5b8fc8c820ca0e021bd9fd
Dmitriy Kasimov <its.dmitriy.kasimov@gmail.com>
2024-07-20 18:52:33 +0300

    initial commit

---
+++ modules/Progress/Progress.js
@@ -0,0 +1,49 @@
+class Progress{
+    // получить длину окружности по формуле 2*Pi*R
+    static getCircumference(radius){
+        return 2 * Math.PI * radius;
+    }
+    // получить дельту между полной длиной окружности и длиной в процентах
+    static getOffset(circumference, percent){
+        return circumference - percent / 100 * circumference;
+    }
+
+    constructor(props){
+        const {
+            selectorProgress, // блок
+            selectorCircle, // прогресс бар
+            selectorValue, // текстовое поле со значением
+            selectorAnimate, // переключатель анимации
+            selectorHide, // переключатель видимости блока
+        } = props;
+
+        this.$circle = document.querySelector(selectorCircle);
+        const radius = this.$circle.r.baseVal.value;
+        const circumference = Progress.getCircumference(radius);
+        this.$circle.style.strokeDasharray = `${circumference} ${circumference}`;
+        this.$circle.style.strokeDashoffset = `${circumference}`;
+        this.$circle.style.transformOrigin = 'center';
+        this.$circle.style.transform = 'rotate(-90deg)';
+        this.$circle.style.transition = 'stroke-dashoffset 0.3s';
+
+        this.$value = document.querySelector(selectorValue);
+        this.$value.addEventListener('change', (e) => {
+            const value = e.target.value;
+            const offset = Progress.getOffset(circumference, value);
+            this.$circle.style.strokeDashoffset = `${offset}`;
+        })
+
+        const $switcherAnimate = document.querySelector(selectorAnimate);
+        $switcherAnimate.addEventListener('change', () => {
+            $switcherAnimate.classList.toggle('switcher-active');
+            this.$circle.classList.toggle('animate');
+        })
+
+        const $switcherHide = document.querySelector(selectorHide);
+        $switcherHide.addEventListener('change', () => {
+            $switcherHide.classList.toggle('switcher-active');
+            const $block = document.querySelector(selectorProgress);
+            $block.classList.toggle('hide');
+        })
+    }
+}
---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+new Progress({
+    selectorProgress: '.progress-ring',
+    selectorCircle: '.progress-ring__circle',
+    selectorValue: '.percent',
+    selectorAnimate: '.animation',
+    selectorHide: '.hidden',
+})
+
+new Progress({
+    selectorProgress: '.progress-ring2',
+    selectorCircle: '.progress-ring__circle2',
+    selectorValue: '.percent2',
+    selectorAnimate: '.animate2',
+    selectorHide: '.hide2',
+})
