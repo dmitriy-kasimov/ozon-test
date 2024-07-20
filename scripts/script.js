0306c2f51ab3e3a2b03013c5e1b5b619fc3cc253
Dmitriy Kasimov <its.dmitriy.kasimov@gmail.com>
2024-07-20 21:23:46 +0300

    fixed label for «Value» text field; removed old object of the Progress class;

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,3 @@ new Progress({
     selectorAnimate: '.animation',
     selectorHide: '.hidden',
 })
-
-new Progress({
-    selectorProgress: '.progress-ring2',
-    selectorCircle: '.progress-ring__circle2',
-    selectorValue: '.percent2',
-    selectorAnimate: '.animate2',
-    selectorHide: '.hide2',
-})
