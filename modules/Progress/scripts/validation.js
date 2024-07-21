be80c1946038661b528a30bbae6b58bb7db4d023
Dmitriy Kasimov <its.dmitriy.kasimov@gmail.com>
2024-07-21 13:17:47 +0300

    added validation and fixed styles for text field of the «Value».

---
+++ modules/Progress/scripts/validation.js
@@ -0,0 +1,11 @@
+const inputs = document.querySelectorAll('.progress-controls__value');
+Array.from(inputs).forEach(input => {
+    const min = +input.min;
+    const max = +input.max;
+
+    input.addEventListener('input', (e) => {
+        const value = +input.value;
+        if (value > max) { input.value = max }
+        else if (value < min) { input.value = min }
+    })
+});
